838e1394c757120277bdb7a9bb4a9a3a0a902693
Xavi <xavi@geekshubs.com>
2019-06-03 12:53:55 +0200

    finish twig feature

--- src/kernel.php
+++ src/kernel.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+
 class kernel
 {
     public function __construct(){
-        echo "<h1>HOLA MUNDO</h1>";
+        $viewManager = new ViewManager();
+       $viewManager->renderTemplate("index.view.html");
     }
 }
