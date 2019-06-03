366e99dc2d724937063151b30996f3fbd99c9c98
Xavi <xavi@geekshubs.com>
2019-06-03 10:59:40 +0200

    finish structure project

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('max_execution_time',0);
+require_once  dirname(__DIR__).'/vendor/autoload.php';
+use App\kernel;
+
+$kernel = new kernel();
---
+++ src/kernel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class kernel
+{
+    public function __construct(){
+        echo "<h1>HOLA MUNDO</h1>";
+    }
+}
