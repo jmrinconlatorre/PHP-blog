7a13d400c46ff12cdadb6edba50a037b117cec2d
Xavi <xavi@geekshubs.com>
2019-06-03 13:31:21 +0200

    finish debugger_logger

--- src/kernel.php
+++ src/kernel.php
@@ -6,6 +6,8 @@ namespace App;
 class kernel
 {
     public function __construct(){
+        $logManager = new LogManager();
+        $logManager->info("Arrancando la aplicación");
         $viewManager = new ViewManager();
        $viewManager->renderTemplate("index.view.html");
     }
