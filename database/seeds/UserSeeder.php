69b90b290a8eb2b669555952331709bc2a9a10a4
juanma <jm.rincon.latorre@gmail.com>
2019-06-05 12:36:13 +0200

    finishing phinx structure

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $data = [];
+        for($i=0;$i<10;$i++){
+            $data[]=[
+                'name'=>$faker->name,
+                'email'=>$faker->name,
+                'password'=> sha1($faker->password),//tipo de encriptación sha1
+                'createdAt'=>date('Y-m-d H:i:s')
+            ];
+        }
+        $this->insert('users',$data);
+    }
+}
