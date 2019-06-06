75bb0f78a77f7dc6029164ee1267c4860c611785
juanma <jm.rincon.latorre@gmail.com>
2019-06-06 12:07:58 +0200

    first commit of blog

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ ini_set('max_execution_time',0);
 require_once  dirname(__DIR__).'/vendor/autoload.php';
 use App\kernel;
 
-session_start();
+session_start();//llamamos a la sesion
 
 $kernel = new kernel();
 $kernel->init();
